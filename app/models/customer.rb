1a6ba1ccfbf669d98f6f738e19fa6bceb02c837a
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-06-04 04:38:53 +0000

    ゲストログイン機能追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :profile_image
-  
+
   has_many :shoes,                    dependent: :destroy
   has_many :keeps,                    dependent: :destroy
   has_many :shoes_comments,           dependent: :destroy
@@ -13,5 +13,30 @@ class Customer < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings,               through: :relationships,            source: :followed
   has_many :followers,                through: :reverse_of_relationships, source: :follower
-  
+
+    enum foot_width: {
+    narrow:     0,
+    standard:   1,
+    wide:       2,
+    not_clear:  3
+  }
+
+  enum foot_type: {
+    egypt_type:   0,
+    greek_type:   1,
+    square_type:  2
+  }
+
+  enum sex: {
+    man:    0,
+    woman:  1,
+  }
+
+
+  def self.guest
+    find_or_create_by!(name: 'guestcustomer', email: 'guest@example.com') do |customer|
+      customer.password = SecureRandom.urlsafe_base64
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,10 @@ Rails.application.routes.draw do
     resources :costomers, only: [:index, :show, :edit, :update]
   end
 
+  devise_scope :customer do
+    post 'customers/guest_sign_in', to: 'customers/sessions#guest_sign_in'
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
